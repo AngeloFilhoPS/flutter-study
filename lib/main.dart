4ea4292f3602000958e1890554d584b2e9f1c6d1
Angelo <angelo.filho@zup.com.br>
2022-02-05 09:28:48 -0400

    imagem por asset local, modulo 2-9

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,18 @@ class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Meu primeiro app flutter'),
-              centerTitle: true,
-            ),
-            body: Column(
-              children: <Widget>[
-                Exemplo(
-                  onPress: () {
-                    print("Clicou no item 1");
-                  },
-                ),
-                Exemplo(
-                    title: "Texto 2",
-                    onPress: () {
-                      print("Clicou no item 2");
-                    })
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Meu primeiro app flutter'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            width: 20,
+            image: AssetImage('assets/google.png'),
+          ),
+        ),
+      ),
+    );
   }
 }
