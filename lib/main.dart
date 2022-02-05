e0fae35d5520b384a9286a770eed0092634b277a
Angelo <angelo.filho@zup.com.br>
2022-02-05 11:41:01 -0400

    exercicio modulo 3-minha versão

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,82 @@ class MeuApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Meu primeiro app flutter'),
+          title: Text('Estadia certa'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(children: <Widget>[
-            Text(
-              "Titulo Legal",
-              style: textStyle,
-            ),
+        body: Column(
+          children: <Widget>[
+            Image.network(
+                "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/jerusalem.jpeg"),
             Container(
-              child: Icon(
-                Icons.star,
-                size: 40,
-                color: Colors.blue,
+              margin: EdgeInsets.all(25),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Column(
+                    children: <Widget>[
+                      Text(
+                        "Kotel",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Jerusalem, Israel",
+                        style: TextStyle(fontWeight: FontWeight.w200),
+                      )
+                    ],
+                  )),
+                  Spacer(),
+                  Expanded(
+                      child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.star,
+                        color: Colors.blue,
+                      ),
+                      Text("3.008")
+                    ],
+                  ))
+                ],
               ),
             ),
-            FlatButton(child: Text("Click aqui"))
-          ]),
+            Container(
+              margin: EdgeInsets.all(25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.call,
+                      color: Colors.blue,
+                    ),
+                    Text("Ligar")
+                  ]),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.blue,
+                    ),
+                    Text("Endereço")
+                  ]),
+                  Column(children: <Widget>[
+                    Icon(
+                      Icons.share_sharp,
+                      color: Colors.blue,
+                    ),
+                    Text("Compartilhar")
+                  ]),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "O Muro das Lamentações ou Muro Ocidental é o segundo local mais sagrado do judaísmo, atrás somente do Santo dos Santos, no monte do Templo. Trata-se do único vestígio do antigo Templo de Herodes, erguido por Herodes, o Grande no lugar do Templo de Jerusalém inicial",
+                  textAlign: TextAlign.justify,
+                ))
+          ],
         ),
       ),
     );
