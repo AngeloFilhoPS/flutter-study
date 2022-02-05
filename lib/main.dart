9ab30e78830acb5c6971c872deb5a8d30f7b43c9
Angelo <angelo.filho@zup.com.br>
2022-02-05 09:32:35 -0400

    imagem por asset internet

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,20 @@ class MeuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Meu primeiro app flutter'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Image(
-            width: 20,
-            image: AssetImage('assets/google.png'),
+          appBar: AppBar(
+            title: Text('Meu primeiro app flutter'),
+            centerTitle: true,
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Image(
+                width: 20,
+                image: AssetImage('assets/google.png'),
+              ),
+              Image.network(
+                  "https://i.pinimg.com/originals/74/4f/47/744f47bad00806bba10eceb1b4d0a845.png")
+            ],
+          )),
     );
   }
 }
