dcc45366c59811cbab4af9216c40fe1a44fe38db
Angelo <angelo.filho@zup.com.br>
2022-02-15 14:40:17 -0400

    refatorando telas modulo 3-10

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview/src/home.dart';
 import 'exemplo.dart';
 
 void main() => runApp(MeuApp());
 
 class MeuApp extends StatelessWidget {
-  var contatos = ['Bonnieky', 'Angelo', 'Victor', 'Melissa'];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Cores'),
+          title: Text('Assina Luxo'),
           centerTitle: true,
         ),
-        body: Center(
-          child: Text("Ola Mundo"),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Cabeçalho'),
-                decoration: BoxDecoration(color: Colors.grey),
-              ),
-            ],
-          ),
-        ),
+        body: HomePage(),
       ),
     );
   }
