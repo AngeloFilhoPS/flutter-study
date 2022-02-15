c46915dc5ca02d9dd3e14353bac619d497698376
Angelo <angelo.filho@zup.com.br>
2022-02-15 14:31:06 -0400

    drawer modulo 3-9

--- lib/main.dart
+++ lib/main.dart
@@ -10,44 +10,24 @@ class MeuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Cores'),
-            centerTitle: true,
+        appBar: AppBar(
+          title: Text('Cores'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Ola Mundo"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Cabeçalho'),
+                decoration: BoxDecoration(color: Colors.grey),
+              ),
+            ],
           ),
-          body: ListView.builder(
-              itemCount: contatos.length,
-              itemBuilder: (ctx, index) {
-                return ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text('${contatos[index]}'),
-                  trailing: FlatButton(
-                    onPressed: null,
-                    child: Text('Ligar'),
-                  ),
-                );
-              })),
+        ),
+      ),
     );
-  }
-}
-
-class Botao extends StatelessWidget {
-  Botao({this.onPress, @required this.icon, @required this.text});
-
-  Function onPress;
-  IconData icon;
-  var text;
-
-  @override
-  Widget build(BuildContext context) {
-    return FlatButton(
-        onPressed: onPress,
-        child: Container(
-            child: Column(children: <Widget>[
-          Icon(
-            icon,
-            color: Colors.blue,
-          ),
-          Text("Ligar")
-        ])));
   }
 }
